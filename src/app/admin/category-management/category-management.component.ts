127039657a5ba95d4eb366da18c996987b7511ad
Kumar <2463661@cognizant.com>
2025-12-20 11:48:20 +0530

    added admin file structure

---
+++ src/app/admin/category-management/category-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-management',
+  imports: [],
+  templateUrl: './category-management.component.html',
+  styleUrl: './category-management.component.css'
+})
+export class CategoryManagementComponent {
+
+}
