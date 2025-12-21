1e1f2ed5540443d837b66d237f53c70609137cf9
Kumar <2463661@cognizant.com>
2025-12-21 17:32:13 +0530

    nuness

---
+++ src/app/admin/admin-reports/admin-reports.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-reports',
+  imports: [],
+  templateUrl: './admin-reports.component.html',
+  styleUrl: './admin-reports.component.css'
+})
+export class AdminReportsComponent {
+
+}
