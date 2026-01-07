13b3b157a724c837d882db623d6a994388f50b4f
LNU <2463714@cognizant.com>
2026-01-07 11:28:09 +0530

    all done

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,3 @@
-// import { bootstrapApplication } from '@angular/platform-browser';
-// import { provideRouter } from '@angular/router';
-// import { AppComponent } from './app/app.component';
-// import { APP_ROUTES } from './app/app.routes';
-
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     provideRouter(APP_ROUTES),
-//   ],
-// }).catch(err => console.error(err));
-
-
 
 import { bootstrapApplication } from '@angular/platform-browser';
 // import { provideRouter } from '@angular/platform-browser'; // Ensure this is @angular/router if using standard routing
