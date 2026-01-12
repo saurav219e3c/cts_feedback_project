a9e2d5ae92e30c8f06e594276310edf5b12990a6
Kumar <2463661@cognizant.com>
2026-01-12 22:09:33 +0530

    dependency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { Routes } from '@angular/router';
 import { AUTH_ROUTES } from './auth/auth.routes';
 import { EMPLOYEE_ROUTES } from './employee/employee.routes';
 import { ADMIN_ROUTES } from './admin/admin.routes';
+import { managerRoutes } from './manager/manager.routes'
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'auth/home-page' }, // optional; if you want app root -> home page
@@ -11,6 +12,7 @@ export const routes: Routes = [
   { path: 'auth', children: AUTH_ROUTES },
   { path: 'employee', children: EMPLOYEE_ROUTES },
   { path: 'admin', children: ADMIN_ROUTES },
+  { path: 'manager', children: managerRoutes },
 
   { path: '**', redirectTo: 'auth/home-page' }, // fallback to home; optional
 ];
