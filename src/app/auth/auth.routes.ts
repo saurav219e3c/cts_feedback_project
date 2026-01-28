eb646a83197dfc3bec6199d1b60797e7cbf91f53
Kumar <2463661@cognizant.com>
2026-01-28 17:19:13 +0530

    get the correction in navigation and fix manual component routing and added loginGuard and inject logout sevice from authservice and give logout in component

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -3,17 +3,18 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { ADMIN_ROUTES } from '../admin/admin.routes';
+import { loginGuard } from '../core/guards/login.guard';
 
 export const AUTH_ROUTES: Routes = [
   // /auth -> redirect to /auth/home-page
   { path: '', pathMatch: 'full', redirectTo: 'home-page' },
 
   // /auth/home-page
-  { path: 'home-page', component: HomePageComponent },
+  { path: 'home-page',canMatch: [loginGuard], component: HomePageComponent },
 
   // /auth/login-page?role=admin|manager|employee
-  { path: 'login-page', component: LoginPageComponent },
+  { path: 'login-page',canMatch: [loginGuard], component: LoginPageComponent },
 
   // /auth/register-page
-  { path: 'register-page', component: RegisterPageComponent },
+  { path: 'register-page',canMatch: [loginGuard], component: RegisterPageComponent },
 ];
